d984c5773b72498a0d5eab9a1fbad3625d3f643f
Mykyta Haranko <mykyta.haranko@cern.ch>
2018-02-06 04:34:10 -0500

    read counters fast for the ssa, also the test pulse init

--- ssa_methods/cal_utility.py
+++ ssa_methods/cal_utility.py
@@ -18,8 +18,7 @@ def init_all():
 	do_phase_tuning()
 	I2C.peri_write('OutPattern7/FIFOconfig', 7)
 	activate_readout_normal()
-
-
+	
 def print_method(name):
 	lines = inspect.getsourcelines(name)
 	print("".join(lines[0]))
@@ -103,3 +102,66 @@ def read_all_lines():
 	for word in lateral_data:
 	    print "--->", '%10s' % bin(to_number(word,8,0)).lstrip('-0b').zfill(8), '%10s' % bin(to_number(word,16,8)).lstrip('-0b').zfill(8), '%10s' % bin(to_number(word,24,16)).lstrip('-0b').zfill(8), '%10s' % bin(to_number(word,32,24)).lstrip('-0b').zfill(8)
 
+
+def start_counters_read():
+    encode_fast_reset = fc7AddrTable.getItem("ctrl_fast_signal_fast_reset").shiftDataToMask(1)
+    encode_orbit_reset = fc7AddrTable.getItem("ctrl_fast_signal_orbit_reset").shiftDataToMask(1)
+    fc7.write("ctrl_fast", encode_fast_reset + encode_orbit_reset)
+
+def read_counters_fast(ssa_first_counter_delay, raw_mode_en = 0):
+	# set the raw mode to the firmware
+	fc7.write("cnfg_phy_slvs_raw_mode_en", raw_mode_en)
+	# write to the I2C
+	I2C.peri_write("AsyncRead_StartDel_MSB", 0)
+	I2C.peri_write("AsyncRead_StartDel_LSB", ssa_first_counter_delay)
+	# check the value
+	if (I2C.peri_read("AsyncRead_StartDel_LSB") != ssa_first_counter_delay):
+		print "Error! I2C did not work properly"
+		exit(1)
+	# ssa set delay of the counters
+	fc7.write("cnfg_phy_slvs_ssa_first_counter_del", ssa_first_counter_delay+25)
+	
+	# counter ready signal
+	mpa_counters_ready = fc7.read("stat_slvs_debug_mpa_counters_ready")
+
+	start_counters_read()
+	while ((mpa_counters_ready == 0)):
+		sleep(0.01)
+		mpa_counters_ready = fc7.read("stat_slvs_debug_mpa_counters_ready")
+	#print "---> MPA Counters Ready(should be one): ", mpa_counters_ready
+	if raw_mode_en == 1:
+		count = np.zeros((20000, ), dtype = np.uint16)
+		for i in range(0,20000):
+			fifo1_word = fc7.read("ctrl_slvs_debug_fifo1_data")
+			fifo2_word = fc7.read("ctrl_slvs_debug_fifo2_data")
+			#print "1: " + bin(reverse_mask(fifo1_word))
+			#print "2: " + bin(reverse_mask(fifo2_word))
+			line1 = to_number(fifo1_word,8,0)
+			line2 = to_number(fifo1_word,16,8)
+			count[i] = (line2 << 8) | line1
+			#print line1, line2
+			if (i%1000 == 0):
+				print "Reading BX #", i
+	else:
+		# here is the parsed mode, when the fpga parses all the counters
+		count = fc7.fifoRead("ctrl_slvs_debug_fifo2_data", 120)
+		# the last counter has to be read over I2C
+		count[119] = I2C.strip_read("ReadCounter_LSB",120)
+
+	sleep(0.1)
+	mpa_counters_ready = fc7.read("stat_slvs_debug_mpa_counters_ready")
+
+	return count
+
+def init_test_pulse():
+	# enable strips
+	I2C.strip_write("ENFLAGS", 0, 0b10101)
+	# init thersholds
+	I2C.peri_write("Bias_THDAC", 255-35)
+	I2C.peri_write("Bias_THDACHIGH", 255-120)
+	# init the dll
+	I2C.peri_write("Bias_DL_en", 1)
+	I2C.peri_write("Bias_DL_ctrl", 1)
+	# init cal pulse itself
+	I2C.peri_write("Bias_CALDAC", 255)
+	I2C.peri_write("CalPulse_duration", 7)
