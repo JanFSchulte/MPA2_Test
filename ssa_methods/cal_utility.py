54e61c4cc5809079acadcf979643e171d64fd224
Mykyta Haranko <mykyta.haranko@cern.ch>
2018-02-08 13:41:44 -0500

    some logical fixes of the code

--- ssa_methods/cal_utility.py
+++ ssa_methods/cal_utility.py
@@ -11,6 +11,7 @@ import inspect
 def init_all():
 	reset()
 	activate_I2C_chip()
+	set_t1_sampling_edge("negative")
 	init_slvs()
 	activate_readout_shift()
 	set_shift_pattern_all(128) 
@@ -43,9 +44,18 @@ def set_t1_sampling_edge(edge):
 def activate_readout_normal():
 	I2C.peri_write('ReadoutMode',0b00)
 
-def activate_readout_async():
+def activate_readout_async(ssa_first_counter_delay = 8):
 	I2C.peri_write('ReadoutMode',0b01)
-
+	# write to the I2C
+	I2C.peri_write("AsyncRead_StartDel_MSB", 0)
+	I2C.peri_write("AsyncRead_StartDel_LSB", ssa_first_counter_delay)
+	# check the value
+	if (I2C.peri_read("AsyncRead_StartDel_LSB") != ssa_first_counter_delay):
+		print "Error! I2C did not work properly"
+		exit(1)
+	# ssa set delay of the counters
+	fc7.write("cnfg_phy_slvs_ssa_first_counter_del", ssa_first_counter_delay+25)
+	
 def activate_readout_shift():
         I2C.peri_write('ReadoutMode',0b10)
 
@@ -73,19 +83,10 @@ def start_counters_read():
     encode_orbit_reset = fc7AddrTable.getItem("ctrl_fast_signal_orbit_reset").shiftDataToMask(1)
     fc7.write("ctrl_fast", encode_fast_reset + encode_orbit_reset)
 
-def read_counters_fast(ssa_first_counter_delay = 8, raw_mode_en = 0):
+def read_counters_fast(raw_mode_en = 0):
 	# set the raw mode to the firmware
 	fc7.write("cnfg_phy_slvs_raw_mode_en", raw_mode_en)
-	# write to the I2C
-	I2C.peri_write("AsyncRead_StartDel_MSB", 0)
-	I2C.peri_write("AsyncRead_StartDel_LSB", ssa_first_counter_delay)
-	# check the value
-	if (I2C.peri_read("AsyncRead_StartDel_LSB") != ssa_first_counter_delay):
-		print "Error! I2C did not work properly"
-		exit(1)
-	# ssa set delay of the counters
-	fc7.write("cnfg_phy_slvs_ssa_first_counter_del", ssa_first_counter_delay+25)
-	
+
 	# counter ready signal
 	mpa_counters_ready = fc7.read("stat_slvs_debug_mpa_counters_ready")
 
@@ -116,7 +117,7 @@ def read_counters_fast(ssa_first_counter_delay = 8, raw_mode_en = 0):
 		# here is the parsed mode, when the fpga parses all the counters
 		count = fc7.fifoRead("ctrl_slvs_debug_fifo2_data", 120)
 		# the last counter has to be read over I2C
-		count[119] = I2C.strip_read("ReadCounter_LSB",120)
+		count[119] = (I2C.strip_read("ReadCounter_MSB", 120) << 8) | I2C.strip_read("ReadCounter_LSB",120)
 
 	sleep(0.1)
 	mpa_counters_ready = fc7.read("stat_slvs_debug_mpa_counters_ready")
@@ -125,9 +126,14 @@ def read_counters_fast(ssa_first_counter_delay = 8, raw_mode_en = 0):
 
 def set_threshold(value):
 	I2C.peri_write("Bias_THDAC", value)
-	if(I2C.peri_read("Bias_THDAC") != value):
+	sleep(0.01)
+	test_read = I2C.peri_read("Bias_THDAC")
+	if(test_read != value):
+		print "Was writing: ", value, ", got: ", test_read
+		I2C.peri_write("Bias_THDAC", value)
+		print I2C.peri_read("Bias_THDAC")
 		print "Error. Failed to set the threshold"
-		error(1)
+		#error(1)
 
 def init_default_thresholds():
 	# init thersholds
@@ -141,24 +147,19 @@ def init_cal_pulse(cal_pulse_amplitude = 255, cal_pulse_duration = 3):
 	I2C.peri_write("Bias_DL_en", 1)
 	I2C.peri_write("Bias_DL_ctrl", 1)
 	# init cal pulse itself
-	I2C.peri_write("Bias_CALDAC", 255)
+	I2C.peri_write("Bias_CALDAC", cal_pulse_amplitude)
 	I2C.peri_write("CalPulse_duration", cal_pulse_duration)
 
-def measure_scurves(nevents = 50, cal_pulse_amplitude = 0):
+def measure_scurves(nevents = 50, cal_pulse_amplitude = 50):
 	# first go to the async mode
 	activate_readout_async()
 	# close shutter and clear counters
 	close_shutter()
 	clear_counters()
-	# configure test pulse if needed
-	if cal_pulse_amplitude > 0:
-		# init chip cal pulse
-		init_cal_pulse(cal_pulse_amplitude, 3)
-		# init firmware cal pulse
-		Configure_TestPulse_MPA_SSA(200, nevents)
-	else:
-		# set the strip enflags (just in case)
-		I2C.strip_write("ENFLAGS", 0, 0b00101)	
+	# init chip cal pulse
+	init_cal_pulse(cal_pulse_amplitude, 3)
+	# init firmware cal pulse
+	Configure_TestPulse_MPA_SSA(200, nevents)
 
 	# then let's try to measure the scurves
 	scurves = np.zeros((256,120), dtype=np.int)
