1aa083b962f138272e3d694d50425c2a3336c432
anookala <anvesh.nookala@cern.ch>
2021-05-25 15:09:22 +0200

    added some doc strings

--- mpa_methods/mpa_cal_utility.py
+++ mpa_methods/mpa_cal_utility.py
@@ -16,6 +16,7 @@ import matplotlib.cm as cm
 import seaborn as sns
 
 class mpa_cal_utility():
+    """ """
 	def __init__(self, mpa, I2C, fc7):
 		# Class instantiation
 		self.mpa = mpa
@@ -26,19 +27,57 @@ class mpa_cal_utility():
 
 # Math functions
 	def errorf(self, x, *p):
+		"""
+
+		:param x: 
+		:param *p: 
+
+		"""
 		a, mu, sigma = p
 		return 0.5*a*(1.0+erf((x-mu)/sigma))
 	def line(self, x, *p):
+		"""
+
+		:param x: 
+		:param *p: 
+
+		"""
 		g, offset = p
 		return  np.array(x) *g + offset
 	def gauss(self, x, *p):
+		"""
+
+		:param x: 
+		:param *p: 
+
+		"""
 		A, mu, sigma = p
 		return A*np.exp(-(x-mu)**2/(2.*sigma**2))
 	def errorfc(self, x, *p):
+		"""
+
+		:param x: 
+		:param *p: 
+
+		"""
 		a, mu, sigma = p
 		return a*0.5*erfc((x-mu)/sigma)
-##### plot_extract_scurve function take scurve data and extract threhsold and noise data. If plot = 1, it also plot scurves and histograms
 	def plot_extract_scurve(self, row, pixel, s_type, scurve, n_pulse, nominal_DAC, start, stop, extract, plot):
+		"""takes scurve data and extracts threshold and noise data. If
+                plot = 1, it also plots scurves and histograms
+
+		:param row: 
+		:param pixel: 
+		:param s_type: 
+		:param scurve: 
+		:param n_pulse: 
+		:param nominal_DAC: 
+		:param start: 
+		:param stop: 
+		:param extract: 
+		:param plot: 
+
+		"""
 		th_array = np.zeros(2040, dtype = np.int )
 		noise_array = np.zeros(2040, dtype = np.float )
 		for r in row:
@@ -84,6 +123,11 @@ class mpa_cal_utility():
 			return 	th_array, noise_array
 # Readout Counters current method
 	def ReadoutCounters(self, raw_mode_en = 0):
+		"""
+
+		:param raw_mode_en:  (Default value = 0)
+
+		"""
 	# set the raw mode to the firmware
 		self.fc7.write("cnfg_phy_slvs_raw_mode_en", raw_mode_en)
 		t0 = time.time()
@@ -131,6 +175,25 @@ class mpa_cal_utility():
 # a = cal.s_curve( n_pulse = 1000, s_type = "CAL", rbr = 0, ref_val = 150, row = range(1,17), step = 1, start = 0, stop = 256, pulse_delay = 500, extract_val = 0, extract = 0, plot = 1, print_file =0 )
 # NB: product n_pulse*pulse_delay > 500e3 to avoid communication problem (IP bus related?)
 	def s_curve(self, n_pulse = 1000, s_type = "THR", rbr = 0, ref_val = 50, row = range(1,17), step = 1, start = 0, stop = 256, pulse_delay = 200, extract_val = 0, extract = 1, plot = 1, print_file =1, filename = "../cernbox/MPA_Results/scurve_fr_"):
+		"""
+
+		:param n_pulse:  (Default value = 1000)
+		:param s_type:  (Default value = "THR")
+		:param rbr:  (Default value = 0)
+		:param ref_val:  (Default value = 50)
+		:param row:  (Default value = range(1)
+		:param 17): 
+		:param step:  (Default value = 1)
+		:param start:  (Default value = 0)
+		:param stop:  (Default value = 256)
+		:param pulse_delay:  (Default value = 200)
+		:param extract_val:  (Default value = 0)
+		:param extract:  (Default value = 1)
+		:param plot:  (Default value = 1)
+		:param print_file:  (Default value = 1)
+		:param filename:  (Default value = "../cernbox/MPA_Results/scurve_fr_")
+
+		"""
 		t0 = time.time()
 		self.fc7.clear_counters(8)
 		row = np.array(row)
@@ -188,6 +251,27 @@ class mpa_cal_utility():
 		else: return data_array
 ### Trimming routine
 	def trimming_step(self, pix_out = [["Row", "Pixel", "DAC"]], n_pulse = 1000, s_type = "THR", ref_val = 10, iteration = 1, nominal_DAC = 110, data_array = np.zeros(2040, dtype = np.int ), plot = 1,  stop = 150, ratio = 3.90, row = range(1,17), pixel = range(1,120)):
+		"""
+
+		:param pix_out:  (Default value = [["Row")
+		:param "Pixel": 
+		:param "DAC"]]: 
+		:param n_pulse:  (Default value = 1000)
+		:param s_type:  (Default value = "THR")
+		:param ref_val:  (Default value = 10)
+		:param iteration:  (Default value = 1)
+		:param nominal_DAC:  (Default value = 110)
+		:param data_array:  (Default value = np.zeros(2040)
+		:param dtype:  (Default value = np.int ))
+		:param plot:  (Default value = 1)
+		:param stop:  (Default value = 150)
+		:param ratio:  (Default value = 3.90)
+		:param row:  (Default value = range(1)
+		:param 17): 
+		:param pixel:  (Default value = range(1)
+		:param 120): 
+
+		"""
 		t0 = time.time()
 		self.mpa.ctrl_pix.load_trim(data_array)
 		try: scurve_init = self.s_curve(n_pulse = n_pulse,  s_type = s_type, rbr = 0, ref_val = ref_val, row = row, step = 1, start = 0, stop = stop, pulse_delay = 500, extract = 0, plot = 0, print_file =0)
@@ -241,6 +325,24 @@ class mpa_cal_utility():
 		return data_array, pix_out
 
 	def trimming_chip(self, s_type = "THR", ref_val = 10, nominal_DAC = 110, nstep = 4, data_array = np.zeros(2040, dtype = np.int ), n_pulse = 300, iteration = 2, extract = 1, plot = 1, stop = 256, ratio = 3.68, print_file = 0, filename = "../cernbox/MPA_Results/Test"):
+		"""
+
+		:param s_type:  (Default value = "THR")
+		:param ref_val:  (Default value = 10)
+		:param nominal_DAC:  (Default value = 110)
+		:param nstep:  (Default value = 4)
+		:param data_array:  (Default value = np.zeros(2040)
+		:param dtype:  (Default value = np.int ))
+		:param n_pulse:  (Default value = 300)
+		:param iteration:  (Default value = 2)
+		:param extract:  (Default value = 1)
+		:param plot:  (Default value = 1)
+		:param stop:  (Default value = 256)
+		:param ratio:  (Default value = 3.68)
+		:param print_file:  (Default value = 0)
+		:param filename:  (Default value = "../cernbox/MPA_Results/Test")
+
+		"""
 		t0 = time.time()
 		pix_out = [["Row", "Pixel", "DAC"]]
 		for i in range(1,nstep+1):
@@ -272,6 +374,19 @@ class mpa_cal_utility():
 		return data_array, pix_out
 # scurve, th, noise, trim, count = cal.trimming_new()
 	def trimming_new(self, ref = 40, low = 80, req = 130, high = 180, nominal_ref = 15, nominal_req = 85 , trim_ampl = -1, rbr = 0, plot = 1):
+		"""
+
+		:param ref:  (Default value = 40)
+		:param low:  (Default value = 80)
+		:param req:  (Default value = 130)
+		:param high:  (Default value = 180)
+		:param nominal_ref:  (Default value = 15)
+		:param nominal_req:  (Default value = 85)
+		:param trim_ampl:  (Default value = -1)
+		:param rbr:  (Default value = 0)
+		:param plot:  (Default value = 1)
+
+		"""
 		t0 = time.time()
 		if trim_ampl > -1:
 			for block in range(0,7):
@@ -314,6 +429,20 @@ class mpa_cal_utility():
 
 # scurve, th, noise, trim, count = cal.trimming_new()
 	def trimming_probe(self, ref = 250, low = 120, req = 90, high = 60, second_step = 1, nominal_ref = 150, nominal_req = 40, trim_ampl = -1, rbr = 0, plot = 1):
+		"""
+
+		:param ref:  (Default value = 250)
+		:param low:  (Default value = 120)
+		:param req:  (Default value = 90)
+		:param high:  (Default value = 60)
+		:param second_step:  (Default value = 1)
+		:param nominal_ref:  (Default value = 150)
+		:param nominal_req:  (Default value = 40)
+		:param trim_ampl:  (Default value = -1)
+		:param rbr:  (Default value = 0)
+		:param plot:  (Default value = 1)
+
+		"""
 		t0 = time.time()
 		if trim_ampl > -1:
 			for block in range(0,7):
