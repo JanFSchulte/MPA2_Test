9a04a4f4e760a66f36e872dbfd7a28e827e6c895
Anvesh Nookala <anvesh.nookala@cern.ch>
2021-07-06 12:10:37 +0200

    load firmware script update

--- load_firmware_mpa-ssa.sh
+++ load_firmware_mpa-ssa.sh
@@ -11,21 +11,22 @@ export LC_ALL=C; unset LANGUAGE
 #   	printf 'IP=%s\n' "$line"
 #   done <"$file"
 
-IP="192.168.1.81"
+IP="192.168.1.79"
 eth=`ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}'`
 
 printf '______________________________________________________\n'
 printf '           Starting SSA-MPA Test System               \n'
 printf '                                                      \n'
 
-cp ./myScripts/ipaddr_mpa.dat  d19cScripts/ipaddr.dat
+cp ./utilities/ipaddr_mpa.dat  d19cScripts/ipaddr.dat
+IP=$(<d19cScripts/ipaddr.dat)
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 ping -c 1 -W 1 $IP; rep=$?
 
 if ! (( $rep == 0 )); then
-	printf   '\n->  SSA Testbench unrichable\n    '
+	printf   '\n->  SSA Testbench unreachable\n    '
 	if (! ifconfig | grep ${eth}:'1'); then
 		read -r -p "    Do you want to configure the communication? [y/N] " response
 		if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
@@ -33,7 +34,7 @@ if ! (( $rep == 0 )); then
 			sudo ifconfig ${eth}:1 192.168.0.100
 			ping -c 1 -W 1 $IP; rep=$?
 			if ! (( $rep == 0 )); then
-				printf '\n->  Testbench unrichable\n'
+				printf '\n->  Testbench unreachable\n'
 			else
 				printf '\n->  Testbench correctly found on %s\n' "$IP"
 			fi
@@ -54,4 +55,4 @@ else
 fi
 
 
-./bin/fc7-d19c.exe  -i $IP -n  d19c_mpa_none_28042019.bin #-f /home/acaratel/D19C/MPA_Test/fw_bitfiles/MPA_SEU_L1_9.bit
+# ./bin/fc7-d19c.exe  -i $IP -n  d19c_mpa_none_28042019.bin #-f /home/acaratel/D19C/MPA_Test/fw_bitfiles/MPA_SEU_L1_9.bit
